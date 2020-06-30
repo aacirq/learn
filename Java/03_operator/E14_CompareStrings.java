2f5ec03e165128e331cb01ecaf50a397807f10b1
Ren Xin <renxin714@outlook.com>
2020-06-30 20:47:59 +0800

    add more cases to 03_operator/E14_

--- Java/03_operator/E14_CompareStrings.java
+++ Java/03_operator/E14_CompareStrings.java
@@ -5,9 +5,10 @@ public class E14_CompareStrings {
         System.out.println(s1 + ".equals(" + s2 + "): " + (s1.equals(s2)));
     }
     public static void main(String[] args) {
-        String s1 = "abc";
+        String s1 = new String("abc");
         String s2 = "abc";
         compare(s1, s2);
+        compare("abc", s1);
         compare("123", "abc");
         compare("abc", "abcd");
     }

53c1ded60241a512bb1b67478d22a5f25b05d0db
Ren Xin <renxin714@outlook.com>
2020-08-22 16:31:31 +0800

    delete redundant files

--- C_C++/C++Primer/c07/e48.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "Sales_data.h"
-
-int main() {
-    std::string null_isbn = "12134";
-    Sales_data item1 = null_isbn;
-    Sales_data item2 = "12345";
-    return 0;
-}
